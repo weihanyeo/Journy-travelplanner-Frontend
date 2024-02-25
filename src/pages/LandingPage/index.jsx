1656f659a8f7ea0c926d73acc624fad3270c361b
longxinyi <intern1@lionsbot.com>
2024-02-25 22:52:59 +0800

    fix: removed nodemon

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,36 +1,49 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 
 const index = () => {
-    return (
-        <div class="container mt-5">
-            <div class="d-flex flex-row-reverse">
-                <button type="button" class="btn btn-primary btn-sm">
-                    Small button
-                </button>
-                <button type="button" class="btn btn-primary">
-                    Left
-                </button>
-            </div>
+  return (
+    <div class="container mt-5">
+      <div class="d-flex flex-row-reverse">
+        <button type="button" class="btn btn-primary btn-sm">
+          Small button
+        </button>
+        <button type="button" class="btn btn-primary">
+          Left
+        </button>
+      </div>
 
-            <div class="mr-6 col-10 p mt-5 container row h-00">
-                <div class="col">
-                    <h1>Embark on Your Journey of Discovery with <b>Journy</b></h1>
-                    <br/>
-                    <h4> Where Adventure Meets Collaboration: </h4> <h4> 
-                        Explore, Share, Create – Your Ultimate Travel Companion
-                    </h4>
-                </div>
-            </div>
-            <br/>
-            <div class="container h-12">
-                <h4> Welcome to Journy, where the world is your playground and every trip is an opportunity for unforgettable memories. Immerse yourself in a community-driven travel experience that goes beyond the ordinary. </h4> <h4> 
-                Welcome to Journy, where the world is your playground and every trip is an opportunity for unforgettable memories. Immerse yourself in a community-driven travel experience that goes beyond the ordinary. Unleash the power of collaboration with our unique social feature, allowing you to effortlessly share, remix, and personalize itineraries created by fellow globetrotters. Fuel your wanderlust, inspire others, and let the journey of a thousand possibilities begin
-                </h4>
-            </div>
+      <div class="mr-6 col-10 p mt-5 container row h-00">
+        <div class="col">
+          <h1>
+            Embark on Your Journey of Discovery with <b>Journy</b>
+          </h1>
+          <br />
+          <h4> Where Adventure Meets Collaboration: </h4>{" "}
+          <h4>Explore, Share, Create – Your Ultimate Travel Companion</h4>
         </div>
-    );
-}
+      </div>
+      <br />
+      <div class="container h-12">
+        <h4>
+          {" "}
+          Welcome to Journy, where the world is your playground and every trip
+          is an opportunity for unforgettable memories. Immerse yourself in a
+          community-driven travel experience that goes beyond the ordinary.{" "}
+        </h4>{" "}
+        <h4>
+          Welcome to Journy, where the world is your playground and every trip
+          is an opportunity for unforgettable memories. Immerse yourself in a
+          community-driven travel experience that goes beyond the ordinary.
+          Unleash the power of collaboration with our unique social feature,
+          allowing you to effortlessly share, remix, and personalize itineraries
+          created by fellow globetrotters. Fuel your wanderlust, inspire others,
+          and let the journey of a thousand possibilities begin
+        </h4>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default index;

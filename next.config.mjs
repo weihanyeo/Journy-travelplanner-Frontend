d2f06c753e58e0f0da687d4c310c88f17fca0b4a
Yeo Wei Han <yeowh@hotmail.sg>
2024-12-29 00:44:45 +0800

    Update next.config.mjs
    Signed-off-by: Yeo Wei Han <yeowh@hotmail.sg>

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Journy-travelplanner-Frontend/' : '',
+  basePath: isProd ? '/Journy-travelplanner-Frontend' : '',
+  output: 'export'
+};
 
+export default nextConfig;
 export default nextConfig;
